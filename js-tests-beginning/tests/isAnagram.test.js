a3718a75187e8f582ef605f03449c0de554cf23f
WoWka <vet-family@yandex.ru>
2024-01-07 23:15:22 +0300

    change error exception to strict as assert

--- js-tests-beginning/tests/isAnagram.test.js
+++ js-tests-beginning/tests/isAnagram.test.js
@@ -1,8 +1,8 @@
 import { isAnagram } from "../src/isAnagram.js";
 import { strict as assert} from 'node:assert';
 
-assert(isAnagram('racer', 'carer') === true);
-assert(isAnagram('', 'crazy') === false);
-assert(isAnagram('crazy', '') === false);
+assert.equal(isAnagram('racer', 'carer'), true);
+assert.equal(isAnagram('', 'crazy'), false);
+assert.equal(isAnagram('crazy', ''), false);
 
 console.log('Tests have been passed!');
